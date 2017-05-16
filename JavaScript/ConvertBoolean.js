953a1e7e778b30e98a08068ac7951a50fdf18a4d
Dominique Ceja <dceja54@gmail.com>
2017-05-15 21:45:52 -0600

    Convert Boolean values

---
+++ JavaScript/ConvertBoolean.js
@@ -0,0 +1,12 @@
+//Conver Boolean Values to Strings 'Yes' or 'No'
+
+/*
+	Given: a boolean value
+
+	Return: a 'Yes' string for true and a 'No' string for false
+
+*/
+
+function boolToWord( bool ){
+   return bool ? 'Yes' : 'No';
+}
